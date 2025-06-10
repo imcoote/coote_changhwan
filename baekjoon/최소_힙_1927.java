39423b3d5107d022ef8dfce3e64ba451801e07c0
chadseok <chadseok@gmail.com>
2025-06-10 15:15:15 +0900

    [최소_힙_1927] BufferedReader로 교체
    메모리: 115140kb -> 32596kb 로 개선
    시간: 1564ms -> 792ms 로 개선

--- baekjoon/최소_힙_1927.java
+++ baekjoon/최소_힙_1927.java
@@ -2,22 +2,23 @@
  * 소요시간: 15분
  * 시간복잡도: O(N logN)
  *      -> 전체 시간복잡도는 힙에 원소를 삽입하고 삭제하는 연산을 N번 반복하므로 O(N log N)이다
- * 메모리: 115140 kb
- * 시간: 1564 ms
+ * 메모리: 32596 kb
+ * 시간: 792 ms
  *******************************************************************************/
 
+import java.io.*;
 import java.util.*;
 
 public class 최소_힙_1927 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = scanner.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
         for (int i = 0; i < N; i++) {
-            int num = scanner.nextInt();
+            int num = Integer.parseInt(br.readLine());
 
             if (num == 0) {
                 if (minHeap.isEmpty()) {
