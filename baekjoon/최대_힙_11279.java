ce49ac368161c88f4df89255b36573843e4ee7e1
chadseok <chadseok@gmail.com>
2025-06-10 15:25:44 +0900

    [최대_힙_11279] BufferedReader로 교체
    메모리: 11076kb -> 32592kb 로 개선
    시간: 1272ms -> 800ms 로 개선

--- baekjoon/최대_힙_11279.java
+++ baekjoon/최대_힙_11279.java
@@ -2,23 +2,23 @@
  * 소요시간: 5분
  * 시간복잡도: O(N logN)
  *      -> 전체 시간복잡도는 힙에 원소를 삽입하고 삭제하는 연산을 N번 반복하므로 O(N log N)이다
- * 메모리: 110076 kb
- * 시간: 1272 ms
+ * 메모리: 32592 kb
+ * 시간: 800 ms
  *******************************************************************************/
 
+import java.io.*;
 import java.util.*;
 
 public class 최대_힙_11279 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int N = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
         // 최소 힙에서 reverseOrder 메서드를 활용해 최대 힙으로 변경
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
         for (int i = 0; i < N; i++) {
-            int num = scanner.nextInt();
+            int num = Integer.parseInt(br.readLine());
 
             if (num == 0) {
                 if (maxHeap.isEmpty()) {
